f8c333667cbafbbcfb2b744a73acc1e677cc7250
Georg Avagyan <georgavagyan@gmail.com>
2020-06-12 16:59:12 +0500

    AG_commit_5

--- catodog/MainApp/urls.py
+++ catodog/MainApp/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import*
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('createReuest/', CreateRequest.as_view, name='CreateRequest_url')
-    path('', Index, name='index')
+    path('', views.index, name='index')
 ]
