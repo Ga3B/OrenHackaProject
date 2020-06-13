f088c7b2697e2f2a41d299b16ffcc42c544929cd
Georg Avagyan <georgavagyan@gmail.com>
2020-06-13 12:39:50 +0500

    Update settings.py

--- catodog/catodog/settings.py
+++ catodog/catodog/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'MainApp.apps.MainappConfig',
+    'social_django',
 
     'django.contrib.sites',
     'allauth',
