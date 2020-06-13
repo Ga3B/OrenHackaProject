cc30730e14dacf945313c1a520b4dbc2dd6f2859
soundofvacuum <soundofvacuum@users.noreply.github.com>
2020-06-13 16:37:54 +0500

    qwe

--- catodog/MainApp/forms.py
+++ catodog/MainApp/forms.py
@@ -1,24 +1,11 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Request
+# from .models import Poll
 from django.db import models
 
 
-# class QueryForm(forms.Form):
-#     # required_css_class = 'required'
-#     color = forms.CharField(label='Окрас', max_length=100)
-#     weight = forms.CharField(label='Вес')
-#     photoUrl = forms.URLField(label='Ссылка на фото')
-#
-
-class RequsetForm(forms.Form):
-    class Meta:
-        model = Request
-        fields = ['dateTime', 'user_id', 'description', 'geotag', 'status', 'photoURL',]
-        widgets = {
-        'user_id': forms.HiddenInput(attrs={'class': 'form-control'}),
-        'description': forms.TimeInput(attrs={'class': 'form-control'}),
-        'geotag': forms.HiddenInput(attrs={'class': 'form-control', 'id': 'geometka'}),
-        'status': forms.HiddenInput(attrs={'class': 'form-control'}),
-        'photoURL': forms.TextInput(attrs={'class': 'form-control'}),
-        }
+class QueryForm(forms.Form):
+    # required_css_class = 'required'
+    color = forms.CharField(label='Окрас', max_length=100)
+    weight = forms.CharField(label='Вес')
+    photoUrl = forms.URLField(label='Ссылка на фото')
