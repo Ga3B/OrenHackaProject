12d3af2fed514f28113a24ecc5378a49b8348714
GreatKing4615 <shahoval99@mail.ru>
2020-06-13 18:39:23 +0500

    forms_comm

--- catodog/MainApp/forms.py
+++ catodog/MainApp/forms.py
@@ -9,3 +9,12 @@ class QueryForm(forms.Form):
     color = forms.CharField(label='Окрас', max_length=100)
     weight = forms.CharField(label='Вес')
     photoUrl = forms.URLField(label='Ссылка на фото')
+
+    
+class RequestForm(forms.Form):
+    # required_css_class = 'required'
+    user_id = forms.HiddenInput()
+    description = forms.CharField(label='Описание', max_length=140)
+    geotag = forms.HiddenInput()
+    status = forms.HiddenInput()
+    photoURL = forms.CharField(label='Фото')
